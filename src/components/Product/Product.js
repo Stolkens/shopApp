c41b15ac14329569ace5648d47087fd52b04949b
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-03-23 08:20:22 +0100

    Add ProductForm component

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,8 @@
 import styles from './Product.module.scss';
-import clsx from 'clsx';
-import Button from '../Button/Button';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import ProductImage from '../ProductImage/ProductImage';
+import ProductForm from '../ProductForm/ProductForm';
 
 
 const Product = ({title, basePrice, colors, sizes, name}) => {
@@ -19,10 +19,6 @@ const Product = ({title, basePrice, colors, sizes, name}) => {
     setCurrentSize(size)
   }
 
-  const prepareColorClassName = color => {
-   return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()]
-  }
-
   const getPrice = basePrice + sizes.find(size => size.name === currentSize).additionalPrice;
 
   const summary = e => {
@@ -37,36 +33,20 @@ const Product = ({title, basePrice, colors, sizes, name}) => {
 
   return (
     <article className={styles.product}>
-      <div className={styles.imageContainer}>
-        <img 
-          className={styles.image}
-          alt={title}
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-${name}--${currentColor}.jpg`} />
-      </div>
+      <ProductImage name={name} currentColor={currentColor} title={title}/>
       <div>
         <header>
           <h2 className={styles.name}>{title}</h2>
           <span className={styles.price}>Price: {getPrice}$</span>
         </header>
-        <form>
-          <div className={styles.sizes}>
-            <h3 className={styles.optionLabel}>Sizes</h3>
-            <ul className={styles.choices}>
-              {sizes.map(size=>
-                <li><button key={size.name} type="button" className={clsx(currentSize === size.name && styles.active)} onClick={()=>handlerSize(size.name)}>{size.name}</button></li>)}
-            </ul>
-          </div>
-          <div className={styles.colors}>
-            <h3 className={styles.optionLabel}>Colors</h3>
-            <ul className={styles.choices}>
-              {colors.map(color=>
-                <li><button key={color} type="button" className={clsx(prepareColorClassName(color), currentColor === color && styles.active)} onClick={()=>handlerColor(color)}></button></li>)}
-            </ul>
-          </div>
-          <Button className={styles.button} >
-            <span className="fa fa-shopping-cart" onClick={(e)=>summary(e)}/>
-          </Button>
-        </form>
+        <ProductForm 
+        sizes={sizes} 
+        currentSize={currentSize} 
+        currentColor={currentColor} 
+        handlerSize={handlerSize} 
+        handlerColor={handlerColor} 
+        summary={summary} 
+        colors={colors}/>
       </div>
     </article>
   )
